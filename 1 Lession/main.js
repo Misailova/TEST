b807230e0df90df42951e49945ef4e8b61227a03
Misailova <130629749+Misailova@users.noreply.github.com>
2023-04-20 16:22:44 +0800

    first commint

--- 1 Lession/main.js
+++ 1 Lession/main.js
@@ -1,3 +1,8 @@
-console.log("Hello World");
-console.log(123);
-console.log("Hello"+ "World");
+//console.log("Hello World");/
+//console.log(123);
+//console.log("Hello"+ "World");
+//alert("World !");
+//document.getElementByld("first").innerHTML="Hello"
+//console.log(11);
+//console.log("Добро"+"Пожаловать"+"на курс"+"Анна");
+let myBirthday="22.08.1996";
